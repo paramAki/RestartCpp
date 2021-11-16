c82340f29e4490395afdd6a22517f74d3315da8d
jingyu.zhou <976944083@qq.com>
2021-11-16 10:12:49 +0800

    -- point and function

--- CppCourse/CppCourse/pointAndArray.cpp
+++ CppCourse/CppCourse/pointAndArray.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main1(){
     int arr[] = {1,2,3,4,5,6,7,8,9};
     int * p = arr;
     
---
+++ CppCourse/CppCourse/pointAndFunction.cpp
@@ -0,0 +1,25 @@
+//
+//  pointAndFunction.cpp
+//  CppCourse
+//
+//  Created by paramAki on 2021/11/16.
+//
+
+#include <iostream>
+using namespace std;
+
+// 地址传递
+void swapAB(int * m, int * n){
+    int temp = *m;
+    *m = *n;
+    *n = temp;
+}
+
+int main(){
+    int a = 1, b = 2;
+    swapAB(&a, &b);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    
+    return 1;
+}
