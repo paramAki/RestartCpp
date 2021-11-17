73b47107211747123c1423639057b5c9923be7bf
jingyu.zhou <976944083@qq.com>
2021-11-17 17:32:08 +0800

    -- cpp core reference as function param

---
+++ CppCore/CppCore/referenceAsParam.cpp
@@ -0,0 +1,26 @@
+//
+//  referenceAsParam.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/17.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//void mySwap(int &a, int &b){
+//    int temp = a;
+//    a = b;
+//    b = temp;
+//}
+//
+//int main(){
+//    int a = 10;
+//    int b = 20;
+//    mySwap(a, b);
+//
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//
+//    return 0;
+//}
--- CppCore/CppCore/referencesBasicSyntax.cpp
+++ CppCore/CppCore/referencesBasicSyntax.cpp
@@ -5,24 +5,24 @@
 //  Created by paramAki on 2021/11/17.
 //
 
-#include <iostream>
-
-using namespace std;
-
-// 引用的作用：给变量起别名
-// 语法：数据类型 &别名 = 原名
-
-int main(){
-    int a = 10;
-    int &b = a;// b引用a
-    
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-    
-    b = 20;
-    
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-    
-    return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//// 引用的作用：给变量起别名
+//// 语法：数据类型 &别名 = 原名
+//
+//int main(){
+//    int a = 10;
+//    int &b = a;// b引用a
+//    
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//    
+//    b = 20;
+//    
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//    
+//    return 0;
+//}
