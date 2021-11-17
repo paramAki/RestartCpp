bc7cd4bda393026ef99f92535b67bf2b782696e7
jingyu.zhou <976944083@qq.com>
2021-11-17 19:58:19 +0800

    -- cpp core function default param

---
+++ CppCore/CppCore/functionDefaultParam.cpp
@@ -0,0 +1,19 @@
+//
+//  functionDefaultParam.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/17.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//void func(int a, int b =  10, int c = 5){
+//    cout << "a + b + c = " << a + b + c << endl;
+//}
+//
+//int main(){
+//    func(10, 20, 40);
+//    return 0;
+//}
