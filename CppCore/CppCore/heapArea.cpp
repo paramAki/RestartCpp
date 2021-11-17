89c84a11195aee276d9d639e735bd1261a67a1c4
jingyu.zhou <976944083@qq.com>
2021-11-17 16:38:52 +0800

    -- cpp core heap area

---
+++ CppCore/CppCore/heapArea.cpp
@@ -0,0 +1,23 @@
+//
+//  heapArea.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/17.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int* func(){
+//    // 利用new关键字，可以将数据开辟到堆区
+//    int * p = new int(10);// new开辟一个地址存的数据是整型10，并返回这个地址
+//    return p;
+//}
+//
+//int main(){
+//    int * p = func();
+//    cout << "*p = " << *p << endl;
+//
+//    return 0;
+//}
