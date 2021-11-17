e998654d2c60927905effb4c2e922040f6a98228
jingyu.zhou <976944083@qq.com>
2021-11-17 09:48:39 +0800

    -- address list manager system add person

--- AddressListManagerSystem/AddressListManagerSystem/main.cpp
+++ AddressListManagerSystem/AddressListManagerSystem/main.cpp
@@ -13,17 +13,17 @@ using namespace std;
 
 // 联系人结构体
 struct Person{
-    string name;
-    int sex;// 性别： 1是男，2是女
-    int age;
-    string phone;
-    string arrdress;
+    string m_Name;
+    int m_Sex;// 性别： 1是男，2是女
+    int m_Age;
+    string m_Phone;
+    string m_Address;
 };
 
 // 通讯录结构体
 struct AddressList{
     Person personArr[MAX];
-    int len;
+    int m_Size;
 };
 
 // 菜单
@@ -39,8 +39,69 @@ void showMenu(){
     cout << "***********************" << endl;
 }
 
+// 添加联系人
+void addPerson(AddressList * als){
+    if(als->m_Size >= MAX){
+        cout << "通讯录已满,无法添加联系人！" << endl;
+        return ;
+    }else{
+        // 姓名
+        string name;
+        cout << "请输入姓名：";
+        cin >> name;
+        als->personArr[als->m_Size].m_Name = name;
+        
+        // 性别
+        int sex;
+        cout << "请输入性别：" << endl;
+        cout << "1 --- 男" << endl;
+        cout << "2 --- 女" << endl;
+        do{
+            cin >> sex;
+            if(sex != 1 && sex != 2){
+                cout << "输入不合法，请重新输入：";
+            }
+        }while(sex != 1 && sex != 2);
+        als->personArr[als->m_Size].m_Sex = sex;
+        
+        // 年龄
+        int age;
+        cout << "请输入年龄：" ;
+        cin >> age;
+        als->personArr[als->m_Size].m_Age = age;
+        
+        // 电话号码
+        string phone;
+        cout << "请输入电话号码：";
+        
+        do{
+            cin >> phone;
+            if(phone.size() != 10)
+                cout << "号码不合法，请重新输入：";
+        }while(phone.size() != 11);
+        als->personArr[als->m_Size].m_Phone = phone;
+        
+        // 住址
+        string address;
+        cout << "请输入地址：";
+        cin >> address;
+        als->personArr[als->m_Size].m_Address = address;
+        
+        // 添加成功
+        als->m_Size++;
+        cout << "添加成功" << endl;
+    }
+}
+
+// 显示联系人
+void showAddressList(AddressList * als){
+    
+}
+
 int main() {
     int select = 0;
+    AddressList als;
+    als.m_Size = 0;
     
     while(true){
         
@@ -50,6 +111,7 @@ int main() {
         
         switch(select){
             case 1:// 1、增加联系人
+                addPerson(&als);
                 break;
             case 2:// 2、显示联系人
                 break;
