e9269ee52d34388f03f039490683d89a66e9b063
jingyu.zhou <976944083@qq.com>
2021-11-17 11:50:55 +0800

    -- address list manager system edit person

--- AddressListManagerSystem/AddressListManagerSystem/main.cpp
+++ AddressListManagerSystem/AddressListManagerSystem/main.cpp
@@ -34,6 +34,8 @@ void showAddressList(AddressList * als);
 int isExist(AddressList * als, string name);
 void searcherPerson(AddressList * als);
 void deletePerson(AddressList * als);
+void editPerson(AddressList * als, int index);
+void modifyPerson(AddressList * als);
 
 int main() {
     int select = 0;
@@ -60,16 +62,18 @@ int main() {
                 searcherPerson(&als);
                 break;
             case 5:// 5、修改联系人
+                modifyPerson(&als);
                 break;
             case 6:// 6、清空联系人
                 break;
             case 0:// 0、退出通讯录
                 cout << "welcome to use address list manager system next time" << endl;
-                cout << "press any key to continue......" << endl;
-                getchar();
+//                cout << "press any key to continue......" << endl;
+//                getchar();
                 return 0;
                 break;
             default:
+                cout << "非法输入，请重新输入!" << endl;
                 break;
         }
         system("clear");
@@ -91,55 +95,101 @@ void showMenu(){
     cout << "***********************" << endl;
 }
 
+// 编辑联系人
+void editPerson(AddressList * als, int index){
+    // 姓名
+    string name;
+    cout << "请输入姓名：";
+    cin >> name;
+    als->personArr[index].m_Name = name;
+    
+    // 性别
+    int sex;
+    cout << "请输入性别：" << endl;
+    cout << "1 --- 男" << endl;
+    cout << "2 --- 女" << endl;
+    do{
+        cin >> sex;
+        if(sex != 1 && sex != 2){
+            cout << "输入不合法，请重新输入：";
+        }
+    }while(sex != 1 && sex != 2);
+    als->personArr[index].m_Sex = sex;
+    
+    // 年龄
+    int age;
+    cout << "请输入年龄：" ;
+    cin >> age;
+    als->personArr[index].m_Age = age;
+    
+    // 电话号码
+    string phone;
+    cout << "请输入电话号码：";
+    
+    do{
+        cin >> phone;
+        if(phone.size() != 11)
+            cout << "号码不合法，请重新输入：";
+    }while(phone.size() != 11);
+    als->personArr[index].m_Phone = phone;
+    
+    // 住址
+    string address;
+    cout << "请输入地址：";
+    cin >> address;
+    als->personArr[index].m_Address = address;
+}
+
 // 添加联系人
 void addPerson(AddressList * als){
     if(als->m_Size >= MAX){
         cout << "通讯录已满,无法添加联系人！" << endl;
         return ;
     }else{
-        // 姓名
-        string name;
-        cout << "请输入姓名：";
-        cin >> name;
-        als->personArr[als->m_Size].m_Name = name;
-        
-        // 性别
-        int sex;
-        cout << "请输入性别：" << endl;
-        cout << "1 --- 男" << endl;
-        cout << "2 --- 女" << endl;
-        do{
-            cin >> sex;
-            if(sex != 1 && sex != 2){
-                cout << "输入不合法，请重新输入：";
-            }
-        }while(sex != 1 && sex != 2);
-        als->personArr[als->m_Size].m_Sex = sex;
-        
-        // 年龄
-        int age;
-        cout << "请输入年龄：" ;
-        cin >> age;
-        als->personArr[als->m_Size].m_Age = age;
-        
-        // 电话号码
-        string phone;
-        cout << "请输入电话号码：";
-        
-        do{
-            cin >> phone;
-            if(phone.size() != 11)
-                cout << "号码不合法，请重新输入：";
-        }while(phone.size() != 11);
-        als->personArr[als->m_Size].m_Phone = phone;
-        
-        // 住址
-        string address;
-        cout << "请输入地址：";
-        cin >> address;
-        als->personArr[als->m_Size].m_Address = address;
+//        // 姓名
+//        string name;
+//        cout << "请输入姓名：";
+//        cin >> name;
+//        als->personArr[als->m_Size].m_Name = name;
+//
+//        // 性别
+//        int sex;
+//        cout << "请输入性别：" << endl;
+//        cout << "1 --- 男" << endl;
+//        cout << "2 --- 女" << endl;
+//        do{
+//            cin >> sex;
+//            if(sex != 1 && sex != 2){
+//                cout << "输入不合法，请重新输入：";
+//            }
+//        }while(sex != 1 && sex != 2);
+//        als->personArr[als->m_Size].m_Sex = sex;
+//
+//        // 年龄
+//        int age;
+//        cout << "请输入年龄：" ;
+//        cin >> age;
+//        als->personArr[als->m_Size].m_Age = age;
+//
+//        // 电话号码
+//        string phone;
+//        cout << "请输入电话号码：";
+//
+//        do{
+//            cin >> phone;
+//            if(phone.size() != 11)
+//                cout << "号码不合法，请重新输入：";
+//        }while(phone.size() != 11);
+//        als->personArr[als->m_Size].m_Phone = phone;
+//
+//        // 住址
+//        string address;
+//        cout << "请输入地址：";
+//        cin >> address;
+//        als->personArr[als->m_Size].m_Address = address;
         
         // 添加成功
+        editPerson(als, als->m_Size);
         als->m_Size++;
         cout << "添加成功" << endl;
     }
@@ -208,3 +258,16 @@ void deletePerson(AddressList * als){
         cout << "删除成功";
     }
 }
+
+void modifyPerson(AddressList * als){
+    string name;
+    cout << "请输入要修改的联系人：";
+    cin >> name;
+    int index = isExist(als, name);
+    if(index == -1){
+        cout << "联系人不存在！" << endl;
+    }else{
+        editPerson(als, index);
+    }
+}
+
