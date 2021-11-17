ce5f83fb4b3296cebb7ff753eabf310c1b6135e4
jingyu.zhou <976944083@qq.com>
2021-11-17 17:58:38 +0800

    -- cpp core reference as return value

---
+++ CppCore/CppCore/referenceAsReturnValue.cpp
@@ -0,0 +1,23 @@
+//
+//  referenceAsReturnValue.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/17.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int& func(){
+//    static int a = 10;
+//    int &b = a;
+//    return b;
+//}
+//
+//int main(){
+//    int a = func();
+//    cout << "a = " << a << endl;
+//    
+//    return 0;
+//}
