f0082440fce10cca5d321d3076590823588fa103
jingyu.zhou <976944083@qq.com>
2021-11-17 19:13:44 +0800

    -- cpp core reference constant

---
+++ CppCore/CppCore/referencecConstant.cpp
@@ -0,0 +1,22 @@
+//
+//  referencecConstant.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/17.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//void printValue(const int &value){
+////    value = 20;
+//    cout << "value = " << value << endl;
+//}
+//
+//int main(){
+//    int a = 10;
+//    printValue(a);
+//    
+//    return 0;
+//}
