a3153b78c8ce8e32e8100463303770d36dd13d48
jingyu.zhou <976944083@qq.com>
2021-11-18 10:20:08 +0800

    -- cpp core function reload

---
+++ CppCore/CppCore/functionReload.cpp
@@ -0,0 +1,34 @@
+//
+//  functionReload.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//void func(){
+//    cout << "func()" << endl;
+//}
+//
+//void func(int a){
+//    cout << "func(int a)" << endl;
+//}
+//
+//void func(int a, double b){
+//    cout << "func(int a, double b)" << endl;
+//}
+//
+//void func(double a, int b){
+//    cout << "func(double a, int b)" << endl;
+//}
+//
+//int main(){
+//    func();
+//    func(10);
+//    func(10, 3.14);
+//    func(3.14, 10);
+//    return 0;
+//}
