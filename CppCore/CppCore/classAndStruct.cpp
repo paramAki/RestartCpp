eba7ca046367cd5be9c22b883ff8dedafa34062a
jingyu.zhou <976944083@qq.com>
2021-11-18 16:12:55 +0800

    -- cpp core the difference of struct and class

---
+++ CppCore/CppCore/classAndStruct.cpp
@@ -0,0 +1,31 @@
+//
+//  classAndStruct.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Class{
+//    int a;
+//};
+//
+//struct Struct{
+//    int b;
+//};
+//
+//int main(){
+//    
+//    // struct和class的区别
+//    // struct的默认权限是public
+//    // class的默认权限是private
+//    Class c1;
+////    c1.a = 1;
+//    
+//    Struct s1;
+//    s1.b = 1;
+//    
+//    return 0;
+//}
