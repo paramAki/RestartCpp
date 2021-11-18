f7ba5688e282b804bf07579cecf3f743fc596b9c
jingyu.zhou <976944083@qq.com>
2021-11-18 15:05:13 +0800

    -- cpp core class and object

---
+++ CppCore/CppCore/classAndObject.cpp
@@ -0,0 +1,27 @@
+//
+//  classAndObject.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//#define PI 3.14
+//
+//using namespace std;
+//
+//class Circle{
+//public:
+//    float radius;
+//    float getPerimeter(){
+//        return PI * 2 * radius;
+//    };
+//};
+//
+//int main(){
+//    Circle c1;
+//    c1.radius = 1;
+//    float per = c1.getPerimeter();
+//    cout << "permimeter = " << per << endl;
+//    return 0;
+//}
