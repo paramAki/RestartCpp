fe221558d9ae71c15470f1a9cb54fc12dc0f87d7
jingyu.zhou <976944083@qq.com>
2021-11-18 15:46:30 +0800

    -- cpp cpre class visit permission

---
+++ CppCore/CppCore/classPermission.cpp
@@ -0,0 +1,33 @@
+//
+//  classPermission.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Person{
+//public:
+//    string p_Name;
+//private:
+//    string p_Secret;
+//protected:
+//    string p_Car;
+//    
+//public:
+//    void func(){
+//        p_Name = "张三";
+//        p_Secret = "****";
+//        p_Car = "拖拉机";
+//    }
+//};
+//
+//int main(){
+//    Person person;
+//    person.p_Name = "李四";
+//    person.p_Secret = "??";
+//    return 0;
+//}
