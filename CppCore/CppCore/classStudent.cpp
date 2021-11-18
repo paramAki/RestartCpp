81cd38d310e61bfa5400d5137fe9fc9e0f0f7285
jingyu.zhou <976944083@qq.com>
2021-11-18 15:28:49 +0800

    -- cpp core class student

---
+++ CppCore/CppCore/classStudent.cpp
@@ -0,0 +1,29 @@
+//
+//  classStudent.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class Student{
+//public:
+//    long s_Id;
+//    string s_Name;
+//
+//    void showInfo(){
+//        cout << "学生：" << s_Name << "\t学号：" << s_Id << endl;
+//    }
+//};
+//
+//int main(){
+//    Student stu;
+//    stu.s_Id = 2201804482;
+//    stu.s_Name = "周靖宇";
+//    stu.showInfo();
+//    return 0;
+//}
