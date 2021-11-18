774627c41e0c248dc35f00512365d135d01a7b07
jingyu.zhou <976944083@qq.com>
2021-11-18 16:58:48 +0800

    -- cpp core public function visit private members

---
+++ CppCore/CppCore/classPrivateMembers.cpp
@@ -0,0 +1,46 @@
+//
+//  classPrivateMembers.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/18.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Person{
+//private:
+//    string m_Name;
+//    int m_Age;
+//
+//public:
+//    void setName(string name){
+//        m_Name = name;
+//    }
+//
+//    string getName(){
+//        return m_Name;
+//    }
+//
+//    void setAge(int age){
+//        m_Age = age;
+//    }
+//
+//    int getAge(){
+//        return m_Age;
+//    }
+//};
+//
+//int main(){
+//    Person p;
+////    p.m_Age = 1;
+//
+//    p.setAge(1);
+//    p.setName("张三");
+//
+//    cout << "name = " << p.getName() << endl;
+//    cout << "age = " << p.getAge() << endl;
+//
+//    return 0;
+//}
