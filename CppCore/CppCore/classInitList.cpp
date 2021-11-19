f387f23a8d0e8fc36f14b56478d9bb85b120c81f
jingyu.zhou <976944083@qq.com>
2021-11-19 20:06:51 +0800

    -- cpp core class initialization list

---
+++ CppCore/CppCore/classInitList.cpp
@@ -0,0 +1,32 @@
+//
+//  classInitList.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/19.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Person{
+//public:
+//    int m_A;
+//    int m_B;
+//    int m_C;
+//    
+//    // 初始化列表，在构造函数前执行
+//    Person(int a, int b, int c) :m_A(a), m_B(b), m_C(c){ }
+//    
+//    void printInfo(){
+//        cout << "m_A = " << m_A << endl;
+//        cout << "m_B = " << m_B << endl;
+//        cout << "m_C = " << m_C << endl;
+//    }
+//    
+//};
+//
+//int main(){
+//    Person p(10, 20, 30);
+//    p.printInfo();
+//    return 0;
+//}
