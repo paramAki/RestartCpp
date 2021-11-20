bd07a9cf18c625f0e4079655613961acbb4579a0
jingyu.zhou <976944083@qq.com>
2021-11-20 17:01:12 +0800

    cpp core class as member of class

---
+++ CppCore/CppCore/classClassAsMemberOfClass.cpp
@@ -0,0 +1,41 @@
+//
+//  classClassAsMemberOfClass.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/20.
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Phone{
+//public:
+//    string m_Brand;
+//    
+//    Phone(string brand){
+//        m_Brand = brand;
+//        cout << "Phone 的构造函数" << endl;
+//    }
+//    ~Phone(){
+//        cout << "Phone 的析构函数" << endl;
+//    }
+//};
+//
+//class Person{
+//public:
+//    string m_Name;
+//    Phone m_Phone;
+//    
+//    Person(string name, string brand) :m_Name(name), m_Phone(brand){
+//        cout << "Person 的构造函数" << endl;
+//    }
+//    ~Person(){
+//        cout << "Person 的析构函数" << endl;
+//    }
+//};
+//
+//int main(){
+//    Person p("张三", "苹果");
+//    
+//    return 0;
+//}
