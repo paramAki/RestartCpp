989331b607eaf8debbaaf7aab4fece6985fd49e6
jingyu.zhou <976944083@qq.com>
2021-11-21 19:45:38 +0800

    -- cpp core class friend

---
+++ CppCore/CppCore/classFriend.cpp
@@ -0,0 +1,38 @@
+//
+//  classFriend.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/21.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Building{
+//    // 代表goodGay可以访问Building中的私有成员
+//    friend void goodGay(Building *building);
+//    
+//public:
+//    Building(){
+//        m_LivingRoom = "客厅";
+//        m_BedRoom = "卧室";
+//    }
+//    
+//public:
+//    string m_LivingRoom;
+//private:
+//    string m_BedRoom;
+//};
+//
+//void goodGay(Building *building){
+//    cout << "正在访问 " << building->m_LivingRoom << endl;
+//    cout << "正在访问 " << building->m_BedRoom << endl;
+//}
+//
+//int main(){
+//    Building building;
+//    goodGay(&building);
+//    
+//    return 0;
+//}
