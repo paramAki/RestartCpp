3062c26ac0740073db1ff34a251afce73b50dfe2
jingyu.zhou <976944083@qq.com>
2021-11-21 15:25:17 +0800

    -- cpp core class static member function

--- CppCore/CppCore/classStaticMembers.cpp
+++ CppCore/CppCore/classStaticMembers.cpp
@@ -12,10 +12,17 @@
 //
 //class Person{
 //public:
+//    int a;
+//    
 //    // 所有对象共享同一份数据；
 //    // 编译阶段就分配内存
 //    // 类内声明，类外初始化操作
 //    static int m_Id;
+//    
+//    // 静态成员函数只能访问静态成员变量
+//    static void func(){
+//        cout << "调用static void func()" << endl;
+//    }
 //};
 //
 //int Person::m_Id = 10;
@@ -28,5 +35,10 @@
 //    p2.m_Id = 20;
 //    cout << "m_Id = " << p1.m_Id << endl;
 //    
+//    Person p;
+//    p.func();
+//    Person::func();
+//    
 //    return 0;
 //}
+ 
