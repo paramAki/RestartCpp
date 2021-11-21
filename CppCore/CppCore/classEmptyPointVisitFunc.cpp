dd265669fc200dc0979927e36e0cbfa907e5238d
jingyu.zhou <976944083@qq.com>
2021-11-21 18:19:10 +0800

    -- cpp core class empty point. visit class member function

---
+++ CppCore/CppCore/classEmptyPointVisitFunc.cpp
@@ -0,0 +1,32 @@
+//
+//  classEmptyPointVisitFunc.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/21.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Person{
+//public:
+//    void showName(){
+//        cout << "thi is a Person class" << endl;
+//    }
+//
+//    void showAge(){
+//        if(this == NULL) return;
+//        cout << "age = " << m_Age << endl;
+//    }
+//private:
+//    int m_Age;
+//};
+//
+//int main(){
+//    Person *p = NULL;
+//    // 空指针访问成员函数
+//    p->showName();
+//    p->showAge();
+//
+//    return 0;
+//}
--- CppCore/CppCore/classThisPoint.cpp
+++ CppCore/CppCore/classThisPoint.cpp
@@ -12,12 +12,12 @@
 //class Person{
 //public:
 //    int m_Age;
-//    
+//
 //    Person(int age){
 //        // this指针指向被调用的成员函数所属的对象
 //        this->m_Age = age;
 //    }
-//    
+//
 //    // 返回引用类型才能返回原来的对象，不然返回的是拷贝的新对象
 //    Person& PersonAdd(Person &p){
 //        this->m_Age += p.m_Age;
@@ -28,12 +28,12 @@
 //int main(){
 //    Person p(10); // this指针指向p
 //    cout << "p.m_Age = " << p.m_Age << endl;
-//    
+//
 //    Person p1(10);
 //    Person p2(10);
 //    p2.PersonAdd(p1).PersonAdd(p1).PersonAdd(p1);
-//    
+//
 //    cout << "p2.m_Age = " << p2.m_Age << endl;
-//    
+//
 //    return 0;
 //}
