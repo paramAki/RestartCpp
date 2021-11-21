b5dc2946443fb088721a46cd931b61e2b889a83f
jingyu.zhou <976944083@qq.com>
2021-11-21 20:16:09 +0800

    -- cpp core class member function as friend

---
+++ CppCore/CppCore/classFunctionAsFriend.cpp
@@ -0,0 +1,53 @@
+//
+//  classAsFriend.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/21.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Building;
+//
+//class GoodGay{
+//public:
+//    GoodGay();
+//
+//public:
+//    void visit();
+//    Building * building;
+//};
+//
+//class Building{
+//
+//    friend void GoodGay::visit();
+//
+//public:
+//    Building(){
+//        m_LivingRoom = "客厅";
+//        m_BedRoom = "卧室";
+//    }
+//
+//public:
+//    string m_LivingRoom;
+//
+//private:
+//    string m_BedRoom;
+//};
+//
+//GoodGay::GoodGay(){
+//    building = new Building;
+//}
+//void GoodGay::visit(){
+//    cout << "正在访问：" << building->m_LivingRoom << endl;
+//    cout << "正在访问：" << building->m_BedRoom << endl;
+//};
+//
+//int main(){
+//    GoodGay gg;
+//    gg.visit();
+//
+//    return 0;
+//}
