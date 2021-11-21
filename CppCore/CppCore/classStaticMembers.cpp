9b92f43cc9a78920ebcc9ee95db83bddf0dab3e2
jingyu.zhou <976944083@qq.com>
2021-11-21 14:57:38 +0800

    -- cpp core class static members

---
+++ CppCore/CppCore/classStaticMembers.cpp
@@ -0,0 +1,32 @@
+//
+//  classStaticMembers.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/20.
+//
+
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class Person{
+//public:
+//    // 所有对象共享同一份数据；
+//    // 编译阶段就分配内存
+//    // 类内声明，类外初始化操作
+//    static int m_Id;
+//};
+//
+//int Person::m_Id = 10;
+//
+//int main(){
+//    Person p1;
+//    cout << "m_Id = " << p1.m_Id << endl;
+//    
+//    Person p2;
+//    p2.m_Id = 20;
+//    cout << "m_Id = " << p1.m_Id << endl;
+//    
+//    return 0;
+//}
