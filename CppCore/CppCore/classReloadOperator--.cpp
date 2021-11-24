87f26a4974618689c435cfc6c40e26e44ef5d086
jingyu.zhou <976944083@qq.com>
2021-11-24 14:56:42 +0800

    -- cpp core class reload operator --

--- CppCore/CppCore/classReloadOperator--.cpp
+++ CppCore/CppCore/classReloadOperator--.cpp
@@ -1,33 +1,30 @@
 //
-//  classReloadOperator++.cpp
+//  classReloadOperator--.cpp
 //  CppCore
 //
-//  Created by paramAki on 2021/11/22.
+//  Created by paramAki on 2021/11/24.
 //
 
 //#include <iostream>
 //using namespace std;
 //
 //class MyInteger{
-//    friend ostream& operator<<(ostream &cout, MyInteger integer);
+//    friend ostream& operator<<(ostream &cout, MyInteger myInteger);
 //public:
 //    MyInteger(){
 //        m_A = 0;
 //    }
 //
-//    // 重载前置++运算符
-//    MyInteger& operator++(){
-//        m_A++;
+//    //  前置--
+//    MyInteger& operator--(){
+//        m_A--;
 //        return *this;
 //    }
 //
-//    // 重载后置++运算符
-//    MyInteger operator++(int){
-//        // 先记录值
+//    // 后置--
+//    MyInteger operator--(int){
 //        MyInteger temp = *this;
-//        // 再递增
-//        m_A++;
-//        // 返回记录结果
+//        m_A--;
 //        return temp;
 //    }
 //
@@ -35,18 +32,20 @@
 //    int m_A;
 //};
 //
-//// 全局函数重载<<
 //ostream& operator<<(ostream &cout, MyInteger myInteger){
 //    cout << myInteger.m_A;
-//
 //    return cout;
 //}
 //
 //int main(){
 //    MyInteger myInteger;
 //    cout << myInteger << endl;
-//    cout << ++myInteger << endl;
-//    cout << myInteger++ << endl;
+//    
+//    cout << --myInteger << endl;
+//    cout << myInteger << endl;
+//
+//    
+//    cout << myInteger-- << endl;
 //    cout << myInteger << endl;
 //
 //    return 0;
