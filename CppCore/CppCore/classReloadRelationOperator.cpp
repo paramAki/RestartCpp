31ec6a2e41800d33d85380885458b7d20625fc7d
jingyu.zhou <976944083@qq.com>
2021-11-24 20:31:21 +0800

    -- cpp core class reload relation operator

---
+++ CppCore/CppCore/classReloadRelationOperator.cpp
@@ -0,0 +1,37 @@
+//
+//  classReloadRelationOperator.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/24.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Person{
+//public:
+//    Person(string name, int age){
+//        m_Name = name;
+//        m_Age = age;
+//    }
+//    
+//    bool operator==(Person &p){
+//        return m_Name == p.m_Name && m_Age == p.m_Age;
+//    }
+//    
+//    bool operator!=(Person &p){
+//        return m_Name != p.m_Name || m_Age != p.m_Age;
+//    }
+//private:
+//    string m_Name;
+//    int m_Age;
+//};
+//
+//int main(){
+//    Person p1("Tom", 19);
+//    Person p2("Tom", 19);
+//    bool b1 = (p1 == p2);
+//    bool b2 = p1 != p2;
+//    return 0;
+//}
