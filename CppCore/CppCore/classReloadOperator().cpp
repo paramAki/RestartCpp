db28c3002201c11df910106fca16350a13f4da66
jingyu.zhou <976944083@qq.com>
2021-11-24 21:15:25 +0800

    -- cpp core class reload operator()

---
+++ CppCore/CppCore/classReloadOperator().cpp
@@ -0,0 +1,36 @@
+//
+//  classReloadOperator().cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/24.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class MyPrint{
+//public:
+//    void operator()(string text){
+//        cout << text << endl;
+//    }
+//};
+//
+//class MyAdd{
+//public:
+//    int operator()(int a, int b){
+//        return a + b;
+//    }
+//};
+//
+//int main(){
+//    MyPrint myPrint;
+//    myPrint("hello world");
+//    
+//    MyAdd myAdd;
+////    int ret = myAdd(10, 20);
+//    // 匿名函数对象
+//    cout << "ret = " << myAdd(10, 20) << endl;
+//    
+//    return 0;
+//}
