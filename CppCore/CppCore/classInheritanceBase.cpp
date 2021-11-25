814da3efa6c58bc70570bfd69e964989bb560884
jingyu.zhou <976944083@qq.com>
2021-11-25 12:07:04 +0800

    -- cpp core class inhereritance base

---
+++ CppCore/CppCore/classInheritanceBase.cpp
@@ -0,0 +1,70 @@
+//
+//  classInheritanceBase.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/25.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Base{
+//public:
+//    void header(){
+//        cout << "header" << endl;
+//    }
+//
+//    void lefter(){
+//        cout << "lefter" << endl;
+//    }
+//
+//    void footer(){
+//        cout << "footer" << endl;
+//    }
+//};
+//
+//class Java : public Base{
+//public:
+//    void content(){
+//        cout << "Java content" << endl << endl;;
+//        cout << "----------------" << endl << endl;
+//    }
+//};
+//
+//class Python : public Base{
+//public:
+//    void content(){
+//        cout << "Python content" << endl << endl;;
+//        cout << "----------------" << endl << endl;
+//    }
+//};
+//
+//class Cpp : public Base{
+//public:
+//    void content(){
+//        cout << "Cpp content" << endl << endl;;
+//        cout << "----------------" << endl << endl;
+//    }
+//};
+//
+//int main(){
+//    Cpp cpp;
+//    cpp.header();
+//    cpp.lefter();
+//    cpp.footer();
+//    cpp.content();
+//
+//    Java java;
+//    java.header();
+//    java.lefter();
+//    java.footer();
+//    java.content();
+//
+//    Python python;
+//    python.header();
+//    python.lefter();
+//    python.footer();
+//    python.content();
+//
+//    return 0;
+//}
