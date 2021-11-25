0ac16760336f831c04ecc7e60b0b4cbbf20d2a69
jingyu.zhou <976944083@qq.com>
2021-11-25 14:03:05 +0800

    -- cpp core class multiple inheritance

---
+++ CppCore/CppCore/classMultipleInheritance.cpp
@@ -0,0 +1,33 @@
+//
+//  classMultipleInheritance.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/25.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Base1{
+//public:
+//    int m_A = 10;
+//};
+//
+//class Base2{
+//public:
+//    int m_A = 20;
+//};
+//
+//class Son : public Base1, public Base2{
+//public:
+//    int m_A = 30;
+//};
+//
+//int main(){
+//    Son s;
+//    cout << "s.m_A = " << s.m_A << endl;
+//    cout << "s.Base1::m_A = " << s.Base1::m_A << endl;
+//    cout << "s.Base2::m_A = " << s.Base2::m_A << endl;
+//    
+//    return 0;
+//}
