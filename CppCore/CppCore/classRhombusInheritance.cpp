dd4305b6549a010548c28a409135013ac2db1058
jingyu.zhou <976944083@qq.com>
2021-11-25 14:22:53 +0800

    -- cpp core class rhombus inheritance

---
+++ CppCore/CppCore/classRhombusInheritance.cpp
@@ -0,0 +1,30 @@
+//
+//  classRhombusInheritance.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/25.
+//
+
+//#include <iostream>
+//using namespace std;
+//
+//class Base{
+//public:
+//    int m_A = 10;
+//};
+//
+//// 利用虚继承解决菱形继承的问题
+//class Base1 : virtual public Base{};
+//
+//class Base2 : virtual public Base{};
+//
+//class Son : public Base1, public Base2{};
+//
+//int main(){
+//    Son s;
+//    s.m_A = 20;
+//    
+//    cout << "s.m_A = " << s.m_A << endl;
+//    
+//    return 0;
+//}
