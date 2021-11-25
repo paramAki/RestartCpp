432118ff6957236569dcff97bdad075bbbdf5fce
jingyu.zhou <976944083@qq.com>
2021-11-25 13:29:45 +0800

    -- cpp ore class inheritance same name menmbers

---
+++ CppCore/CppCore/classInheritanceSameNameMembers.cpp
@@ -0,0 +1,39 @@
+//
+//  classInheritanceSameNameMembers.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/25.
+//
+
+//#include <iostream>
+//
+//using namespace  std;
+//
+//class Basa{
+//public:
+//    void func(){
+//        cout << "base function" << endl;
+//        cout << "m_A = " << m_A << endl;
+//    }
+//private:
+//    int m_A = 100;
+//};
+//
+//class Son : public Basa{
+//public:
+//    void func(){
+//        cout << "son function" << endl;
+//        cout << "m_A = " << m_A << endl;
+//    }
+//private:
+//    int m_A = 200;
+//};
+//
+//int main(){
+//    Son s;
+//    s.func();
+//
+//    s.Basa::func();
+//
+//    return 0;
+//}
