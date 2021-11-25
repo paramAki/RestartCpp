a733daa45fddaf03187fd289e9a354a65e5bd2b8
jingyu.zhou <976944083@qq.com>
2021-11-25 13:01:34 +0800

    -- cpp core class inheritance constructors and destructors

---
+++ CppCore/CppCore/classInheritanceConstructorsAndDestructors.cpp
@@ -0,0 +1,36 @@
+//
+//  classInheritanceConstructorsAndDestructors.cpp
+//  CppCore
+//
+//  Created by paramAki on 2021/11/25.
+//
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Base{
+//public:
+//    Base(){
+//        cout << "base 构造函数 " << endl;
+//    }
+//    ~Base(){
+//        cout << "base 析构函数 " << endl;
+//    }
+//};
+//
+//class Son : public Base{
+//public:
+//    Son(){
+//        cout << "son 构造函数 " << endl;
+//    }
+//    ~Son(){
+//        cout << "son 析构函数 " << endl;
+//    }
+//};
+//
+//int main(){
+//    Son s;
+//    
+//    return 0;
+//}
